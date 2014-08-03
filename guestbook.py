0311cc31e47c762cd0e1fa39a0220c251b28c0fd
Seulkiming <conductor@tutti.kr>
2014-08-03 23:16:13 +0900

    created guestbook.pyc

--- guestbook.py
+++ guestbook.py
@@ -1,20 +1,32 @@
+import cgi
+import webapp2
 from google.appengine.api import users
 
 
-import webapp2
+MAIN_PAGE_HTML = """\
+<html>
+    <body>
+        <form action="/sign" method="post">
+            <div><textarea name="content" cols="60" rows="3"></textarea></div>
+            <div><input type="submit" value="Sign Guestbook"></div>
+        </form>
+    </body>
+</html>
+"""
 
 
 #this code defines one request handler, MainPage, mapped to the root URL(/)
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        #checks for active Google account session
-        user = users.get_current_user()
-        if user:
-            self.response.headers["Content-Type"] = "text/plain"
-            self.response.write("Hello! " + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        self.response.write(MAIN_PAGE_HTML)
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.write("<html><body>You wrote:<pre>")
+        self.response.write(cgi.escape(self.request.get("content")))
+        self.response.write("</pre></body></html>")
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/sign', Guestbook),
 ], debug=True)
